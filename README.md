814a921fcb82060b822f9d8d052d8340d1bc2b28
Boom455 <boomchooboom@gmail.com>
2023-11-29 11:15:44 +0700

    add btn

--- README.md
+++ README.md
@@ -13,7 +13,8 @@
 ### 2023-11-29
 1. สร้าง repositories ใหม่ชื่อว่า"DIP_calculator" + README ด้วย
 2. clone repositories มาลงใน VSCode
-3. เขียน note ใน README ว่าทำอะไรไปบ้างในวันนี้ และวันต่อไปด้วย
+3. โหลด python extension
+4. เขียน note ใน README ว่าทำอะไรไปบ้างในวันนี้ และวันต่อไปด้วย
 
 ## Disclaimer
 for educational purpose only. Run on your own risk.
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-from tkinter import *
+import tkinter as tk    
+
+class MyCalculator:
+    def __init__(self):
+
+        self.root = tk.Tk()
+
+        self.root.geometry("300x300")
+        self.root.title("My Calculator")
+
+        self.label = tk.Label(self.root, text="Hello DIP01", font=("Arial", 18))
+        self.label.pack()
+
+        self.button = tk.Button(self.root, text="AC", width=5, height=2)
+        self.button.place(x=40, y=40)
+
+        self.button = tk.Button(self.root, text="+/-", width=5, height=2)
+        self.button.place(x=85, y=40)
+        
+        self.button = tk.Button(self.root, text="%", width=5, height=2)
+        self.button.place(x=130 ,y=40)
+
+        self.root.mainloop()
+
+MyCalculator()
